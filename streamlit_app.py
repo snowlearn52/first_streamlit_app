0fe90787b75c7707ae8c391171936f41cf43b135
MellyMeI Melnyk <118526106+snowlearn52@users.noreply.github.com>
2022-12-21 21:57:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#New Section to display FruityVice api responses
+streamlit.header("Fruityvice Fruit Advice!")
+
