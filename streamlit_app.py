d9918817185d070d1fc6cbfba072740c4b404bc8
MellyMeI Melnyk <118526106+snowlearn52@users.noreply.github.com>
2022-12-27 22:49:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-my_cur = my_cnx.cursor()
+#my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruityvice")
 streamlit.header("The fruitload list contains:")
 #Snowflake-related functions
