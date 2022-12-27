e1f1005e6cd95c4ccca32a9365f29edc1cd2410f
MellyMeI Melnyk <118526106+snowlearn52@users.noreply.github.com>
2022-12-27 23:01:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 #my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruityvice")
 streamlit.header("The fruitload list contains:")
